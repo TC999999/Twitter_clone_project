a2b0740dfedc7491f7bc45242d8bc032c5694b5d
Tyler <insertcleverusernamehere999@gmail.com>
2024-02-14 17:14:07 -0500

    deployment build (dev secret key not included)

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sqlalchemy.exc import IntegrityError
 
 from forms import UserAddForm, LoginForm, MessageForm, EditUserForm
 from models import db, connect_db, User, Message
-APP_SECRET_KEY="526456u789OIUYT158dgsfhjklOIPUYTRE"
+
 
 CURR_USER_KEY = "curr_user"
 
@@ -21,7 +21,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = False
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", APP_SECRET_KEY)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
