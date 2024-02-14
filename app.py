8ec657de7f3e361a187ecf614d67687ae1afee46
Tyler <insertcleverusernamehere999@gmail.com>
2024-02-14 17:16:51 -0500

    development build (dev secret key included in .gitignore)

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from sqlalchemy.exc import IntegrityError
 
 from forms import UserAddForm, LoginForm, MessageForm, EditUserForm
 from models import db, connect_db, User, Message
+from secret_word import APP_SECRET_KEY
 
 
 CURR_USER_KEY = "curr_user"
@@ -21,7 +22,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = False
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY",APP_SECRET_KEY)
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
